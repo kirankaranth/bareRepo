b611da6fd18c235aa938c68ee9762bf87c73f33b
kiran@simpledatalabs.com <kiran@simpledatalabs.com>
2023-07-17 08:26:22 +0000

    ❇️ New     - UDF        - test_udf
    🔁 Modified
        - Pipeline
           - p1

--- udfmerge/pipelines/p1/code/p1/udfs/UDFs.py
+++ udfmerge/pipelines/p1/code/p1/udfs/UDFs.py
@@ -14,8 +14,24 @@ from prophecy.lookups import (
 )
 
 def registerUDFs(spark: SparkSession):
+    spark.udf.register("test_udf", test_udf)
+    
+
     try:
         from prophecy.utils import ScalaUtil
         ScalaUtil.initializeUDFs(spark)
     except :
         pass
+
+def test_udfGenerator():
+    initial = 10
+
+    @udf(returnType = IntegerType())
+    def func(input):
+        input = int(input) if input is not None else 2
+
+        return int(input) * int(input) if input is not None else initial
+
+    return func
+
+test_udf = test_udfGenerator()
